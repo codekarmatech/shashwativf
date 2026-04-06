a94fb79f744ff3143be48de24a0b8cf4a8ef955e
PranshuDixitDev <codingbullz@gmail.com>
2026-04-06 12:38:23 +0530

    feat: add STATIC_ROOT and MEDIA_ROOT to decouple configuration

--- backend/shashwativf_backend/settings.py
+++ backend/shashwativf_backend/settings.py
@@ -135,11 +135,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = config('STATIC_ROOT', default=os.path.join(BASE_DIR, 'staticfiles'))
 
 # Media files (User uploaded content)
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = config('MEDIA_ROOT', default=os.path.join(BASE_DIR, 'media'))
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
